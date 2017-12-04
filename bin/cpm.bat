17a96de9e8962d5cf792eb2b6ccf19075ac7a0c2
John Barry <john.barry@cellarise.com>
2017-12-05 08:04:42 +1000

    MDBLDGEN-106 - update cpm batch to allow for 2 more argument variables

--- bin/cpm.bat
+++ bin/cpm.bat
@@ -1,1 +1,1 @@
-call "%~dp0"cpm\%1.bat "%cd%" %2 %3
+call "%~dp0"cpm\%1.bat "%cd%" %2 %3 %4 %5
--- bin/cpm/func.bat
+++ bin/cpm/func.bat
@@ -1,2 +1,2 @@
 set path = c:\OpenSSL-Win32\bin;%windir%\Microsoft.NET\Framework\v4.0.30319;%path%
-call node  "%~dp0\..\node_modules\azure-functions-core-tools\lib\main.js" %2 %3
+call node  "%~dp0\..\node_modules\azure-functions-core-tools\lib\main.js" %2 %3 %4 %5
