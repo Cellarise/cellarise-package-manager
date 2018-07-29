207af7b8c7debe0cc3bae99d3cacc3919ecbc20a
John Barry <john.barry@nhvr.gov.au>
2018-07-29 17:55:15 +1000

    Add packages: eslint-plugin-react-native; j-pdfjson;retire;react-native-cli; Add cpm commands for adb and react-native

---
+++ bin/cpm/adb.bat
@@ -0,0 +1,2 @@
+set path = %USERPROFILE%\AppData\Local\Android\Sdk\platform-tools;%path%
+call adb %2 %3 %4 %5
---
+++ bin/cpm/react-native.bat
@@ -0,0 +1,1 @@
+call node "%~dp0\..\node_modules\react-native-cli\index.js" %2 %3 %4 %5
