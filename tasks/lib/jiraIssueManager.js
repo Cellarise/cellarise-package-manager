663798d22df3533823afc21250967a7b07086dd3
John Barry <john.barry@nhvr.gov.au>
2019-04-08 12:00:34 +1000

    Fix eslint errors; Remove package-lock

--- tasks/lib/jiraIssueManager.js
+++ tasks/lib/jiraIssueManager.js
@@ -1,62 +1,59 @@
 "use strict";
 
-const jiraConnector = require("jira-connector");
+const JiraConnector = require("jira-connector");
 const jiraConfig = require("../../lib/utils/config")("jira");
 const R = require("ramda");
 
 /**
- * Creates and authenticates a new Jira Connector Client. 
- * @param {Function} callback 
- * @returns {Object} Authenticated Jira Client.
+ * Creates and authenticates a new Jira Connector Client.
+ * @param {Function} callback - Jira Oauth Connector Client
  */
 const getJiraOauthClient = function(callback) {
-    callback(null, 
-      new jiraConnector({
-        host: jiraConfig.host,
-        oauth: {
-          consumer_key: jiraConfig.oauth.consumer_key,
-          private_key: jiraConfig.oauth.consumer_secret.replace(new RegExp("\\\\n", "\g"), "\n"),
-          token: jiraConfig.oauth.access_token,
-          token_secret: jiraConfig.oauth.access_token_secret
+    callback(null,
+      new JiraConnector({
+        "host": jiraConfig.host,
+        "oauth": {
+          "consumer_key": jiraConfig.oauth.consumer_key,
+          "private_key": jiraConfig.oauth.consumer_secret.replace(new RegExp("\\\\n", "g"), "\n"),
+          "token": jiraConfig.oauth.access_token,
+          "token_secret": jiraConfig.oauth.access_token_secret
         }
       })
-    ); 
+    );
 };
 
 /**
  * Extracts a Jira issue key from a current bamboo branch name based on a provided regex.
- * This issue key is used to query the current issue state and all related information. 
- * @param {Object} jiraOauthClient 
- * @param {Function} callback 
- * @returns {Object} Jira Issue.
+ * This issue key is used to query the current issue state and all related information.
+ * @param {Object} jiraOauthClient - Jira Oauth Connector Client
+ * @param {Function} callback - Jira Issue.
  */
 const getJiraIssue = function(jiraOauthClient, callback) {
     const JIRA_ISSUE_KEY = process.env.bamboo_repository_git_branch.match(new RegExp(jiraConfig.stories.regex, "g"));
-    
+
     if (R.isEmpty(JIRA_ISSUE_KEY) || R.isNil(JIRA_ISSUE_KEY) || R.isNil(jiraOauthClient)) {
         callback(null, null);
         return;
     }
-    
+
     jiraOauthClient.issue.getIssue({
-        issueKey: JIRA_ISSUE_KEY
-    
+        "issueKey": JIRA_ISSUE_KEY
+
     }, function(error, issue) {
-    
+
         if (!R.isEmpty(error) && !R.isNil(error)) {
             callback(error);
             return;
         }
-    
+
         callback(null, issue);
     });
 };
 
 /**
  * Analyses components of a supplied issue and extract potential test cases out of them.
- * @param {Object} issue 
- * @param {Function} callback 
- * @returns {String} A single or multiple (concatenated) test cases.
+ * @param {Object} issue - Jira issue
+ * @param {Function} callback - A single or multiple (concatenated) test cases.
  */
 const getJiraTestCasesToRun = (issue, callback) => {
 
@@ -64,34 +61,34 @@ const getJiraTestCasesToRun = (issue, callback) => {
         callback(null, null);
         return;
     }
-    
-    const components = issue.fields.components; 
-    
+
+    const components = issue.fields.components;
+
     if (R.isEmpty(components)) {
         callback(null, null);
         return;
     }
-    
+
     let componentsNames = '';
-    
+
     components.forEach((component) => {
-    
+
         // Replace all spaces and words after last number. Any space before numbers replace with _.
         let currentComponent = component.name.replace(/\s([a-zA-Z])+/g, '').replace(/[\s]+/g,'_');
-        
+
         if (R.isEmpty(componentsNames)) {
             componentsNames = currentComponent;
-        
+
         } else {
             componentsNames += ':' + currentComponent;
         }
     });
-    
+
     callback(null, componentsNames);
 };
 
 module.exports = {
-    getJiraOauthClient: getJiraOauthClient,
-    getJiraIssue: getJiraIssue,
-    getJiraTestCasesToRun: getJiraTestCasesToRun
+    "getJiraOauthClient": getJiraOauthClient,
+    "getJiraIssue": getJiraIssue,
+    "getJiraTestCasesToRun": getJiraTestCasesToRun
 };
